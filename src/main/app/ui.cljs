b4c7080b268d674c253de53476887ad32fae0da8
Spencer Apple <spencer@adstage.io>
2019-10-27 11:09:27 -0700

    Move animations into garden-css

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -1,4 +1,5 @@
 (ns app.ui
+  (:require-macros [garden.def :refer [defkeyframes]])
   (:require
    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
    [com.fulcrologic.fulcro.dom :as dom]
@@ -7,55 +8,64 @@
    [app.mutations :as api]
    [app.util :refer-macros [inspect]]
    [app.graphics :as graphics]
+   [garden.core :as g]
    ))
 
-;; start from nothing
-;; clicker
-;; click the single button
-;; and augs could be added to a different element
-;; or new things could appear
-;; 
+(defkeyframes meow
+  [:from {:opacity 0}]
+  [:to {:opacity 1}])
 
-;; okay so
-;; rooms consist of a wormhole that can be open to one other place
-;; hovering or clicking on a wormhole shows the neighboring rooms
-;; when your wormhole is open, clicking on a different room connects the two rooms
-;;   and opens a new browser tab
-;; when that browser tab closes, the wormhole connection breaks
-
-;; a single room may only have one wormhole open to another room
-;; to close the wormhole, you need to close the other browser tab
-
-;; items can live in a room
-;; items can be clicked and dragged into a wormhole (and they go through the wormhole)
-;;
-;; first level
-;; you only have two things:
-;; one wormhole mouth
-;; one keycard slot
-;; 
+(defkeyframes rotate
+  [:from {:transform       "rotate(0deg)"
+          :transform-origin "48.5% 49%"}]
+  [:to {:transform         "rotate(360deg)"
+        :transform-origin "48.5% 49%"}])
 
 (def spiral-length 2161.78125)
 
-(def orig-path "M 78.364487,212.2036 c 0.284267,1.59189 -2.005595,1.16857 -2.645834,0.47247 -1.735007,-1.88639 -0.232517,-4.74557 1.700893,-5.76414 3.458415,-1.82198 7.51609,0.51027 8.882441,3.87426 2.005174,4.93679 -1.25027,10.33316 -6.047618,12.00074 -6.39411,2.22263 -13.167925,-1.9876 -15.119049,-8.22098 -2.45529,-7.84407 2.723253,-16.01141 10.394343,-18.23735 9.290718,-2.69592 18.859867,3.45787 21.355657,12.5677 2.941202,10.73563 -4.191835,21.71143 -14.741069,24.47396 -12.179508,3.18946 -24.565066,-4.92536 -27.592264,-16.91443 -3.439695,-13.62274 5.658582,-27.42015 19.087794,-30.71056 15.065546,-3.69135 30.276309,6.39158 33.828869,21.26115 3.94403,16.50804 -7.12443,33.13325 -23.434516,36.94718 -17.950315,4.19747 -35.990805,-7.85715 -40.06548,-25.60788 -4.451524,-19.39242 8.589772,-38.84885 27.781245,-43.18379 20.834396,-4.70604 41.707261,9.32232 46.302091,29.95461 4.96094,22.27627 -10.05481,44.566 -32.127974,49.42039 -23.718067,5.21615 -47.424991,-10.78725 -52.538695,-34.30133 -5.471603,-25.1598 11.519645,-50.2842 36.474695,-55.657 26.60147,-5.72727 53.143594,12.25201 58.775304,38.64806 5.98312,28.0431 -12.98435,56.00313 -40.821422,61.8936 -29.484691,6.23912 -58.862809,-13.71665 -65.01191,-42.99478 -6.495247,-30.92625 14.448944,-61.7226 45.168146,-68.13021 32.367786,-6.75149 64.582486,15.18121 71.248516,47.3415 7.00783,33.8093 -15.91347,67.44247 -49.51487,74.36683 -35.250778,7.26425 -70.302516,-16.64572 -77.485126,-51.68824 -7.5207508,-36.69225 17.377945,-73.16263 53.861598,-80.60343 38.133698,-7.77732 76.022818,18.11017 83.721738,56.03496 8.03394,39.57514 -18.84238,78.88305 -58.208327,86.84004 C 50.577096,294.57755 9.8503282,266.71236 1.6353212,225.90524 -6.912028,183.44726 21.942089,141.30158 64.19037,132.8286 c 43.89939,-8.80412 87.46402,21.03895 96.19495,64.72841 9.06093,45.34078 -21.77114,90.32443 -66.901775,99.31325")
+(defonce spiral-path
+  (-> "M 78.364487,212.2036 c 0.284267,1.59189 -2.005595,1.16857 -2.645834,0.47247 -1.735007,-1.88639 -0.232517,-4.74557 1.700893,-5.76414 3.458415,-1.82198 7.51609,0.51027 8.882441,3.87426 2.005174,4.93679 -1.25027,10.33316 -6.047618,12.00074 -6.39411,2.22263 -13.167925,-1.9876 -15.119049,-8.22098 -2.45529,-7.84407 2.723253,-16.01141 10.394343,-18.23735 9.290718,-2.69592 18.859867,3.45787 21.355657,12.5677 2.941202,10.73563 -4.191835,21.71143 -14.741069,24.47396 -12.179508,3.18946 -24.565066,-4.92536 -27.592264,-16.91443 -3.439695,-13.62274 5.658582,-27.42015 19.087794,-30.71056 15.065546,-3.69135 30.276309,6.39158 33.828869,21.26115 3.94403,16.50804 -7.12443,33.13325 -23.434516,36.94718 -17.950315,4.19747 -35.990805,-7.85715 -40.06548,-25.60788 -4.451524,-19.39242 8.589772,-38.84885 27.781245,-43.18379 20.834396,-4.70604 41.707261,9.32232 46.302091,29.95461 4.96094,22.27627 -10.05481,44.566 -32.127974,49.42039 -23.718067,5.21615 -47.424991,-10.78725 -52.538695,-34.30133 -5.471603,-25.1598 11.519645,-50.2842 36.474695,-55.657 26.60147,-5.72727 53.143594,12.25201 58.775304,38.64806 5.98312,28.0431 -12.98435,56.00313 -40.821422,61.8936 -29.484691,6.23912 -58.862809,-13.71665 -65.01191,-42.99478 -6.495247,-30.92625 14.448944,-61.7226 45.168146,-68.13021 32.367786,-6.75149 64.582486,15.18121 71.248516,47.3415 7.00783,33.8093 -15.91347,67.44247 -49.51487,74.36683 -35.250778,7.26425 -70.302516,-16.64572 -77.485126,-51.68824 -7.5207508,-36.69225 17.377945,-73.16263 53.861598,-80.60343 38.133698,-7.77732 76.022818,18.11017 83.721738,56.03496 8.03394,39.57514 -18.84238,78.88305 -58.208327,86.84004 C 50.577096,294.57755 9.8503282,266.71236 1.6353212,225.90524 -6.912028,183.44726 21.942089,141.30158 64.19037,132.8286 c 43.89939,-8.80412 87.46402,21.03895 96.19495,64.72841 9.06093,45.34078 -21.77114,90.32443 -66.901775,99.31325"
+    (graphics/translate-path-str {:y-translation -130.67721})))
 
-(defn spiral [path-classes]
-  (dom/svg {:width "100%"
-            :viewBox "0 0 162.61878 166.3229"
-            :id "spiral"}
-    (dom/g
-      (dom/path {:style {"fill" "none",
-                         "fill-rule" "evenodd",
-                         "stroke-width" "0.26458332px",
-                         "stroke-linejoin" "miter",
-                         "stroke-opacity" "1",
-                         "stroke" "#000000",
-                         "stroke-linecap" "butt"
-                         "stroke-dasharray" "200"
-                         "stroke-dashoffset" "100"}
-                 :classes path-classes
-                 :d (graphics/translate-path-str orig-path
-                      {:y-translation -130.67721})}))))
+(defsc Wormhole [this
+                 {:keys [wormhole/status] :as props}
+                 {:keys [on-wormhole-click] :as computed}
+                 {:as css}
+                 ]
+  {:query [:wormhole/status]
+   :css [
+         #_[:$wormhole-activated-animation
+            {
+             ;;:transition "stroke-dashoffset 2s ease-in-out"
+             }
+            ]
+         [:$spiral-wrapper
+          {:height "80%"
+           :padding "5px"}]
+         [:$wormhole-activated-animation
+          ^:prefix {:animation (str "rotate 2s infinite linear," "meow 2s infinite linear")}]
+         meow
+         rotate]}
+  (let [path-classes ["wormhole-activated-animation"]]
+    (dom/div {:onClick on-wormhole-click}
+      (dom/div :.spiral-wrapper
+        (dom/svg {:width "100%"
+                  :viewBox "0 0 162.61878 166.3229"
+                  :id "spiral"}
+          (dom/g
+            (dom/path {:style {"fill" "none",
+                               "fill-rule" "evenodd",
+                               "stroke-width" "0.26458332px",
+                               "stroke-linejoin" "miter",
+                               "stroke-opacity" "1",
+                               "stroke" "#000000",
+                               "stroke-linecap" "butt"
+                               "stroke-dasharray" "200"
+                               "stroke-dashoffset" "100"}
+                       :classes path-classes
+                       :d spiral-path})))))))
+
+(def ui-wormhole (comp/factory Wormhole))
 
 (defn animate-spiral [spiral-ele]
   (let [length (.getTotalLength spiral-ele)
@@ -138,7 +148,10 @@
            :room/items
            :room/unaccessible?
            :wormhole/status
-           :wormhole/connected]
+           :wormhole/connected
+           ;; dirty hack to get the wormhole css added
+           {:wormhole (comp/get-query Wormhole)}
+           ]
    :ident (fn [] (api/room-ident id))
    :css [[:.basic-style
           {:color "black"
@@ -157,13 +170,7 @@
            :--aug-br "15px"}]
          [:.wormhole-opened
           {:--aug-border-bg "linear-gradient(red, transparent), linear-gradient(to right, blue, transparent), black"}]
-         #_[:$wormhole-activated-animation
-          {
-           ;;:transition "stroke-dashoffset 2s ease-in-out"
-           }
-          ]
-         [:$wormhole-activated-animation
-          {:animation "activated 2s infinite linear"}]
+
          
          [:.room-unaccessible
           {:opacity 0.5}]
@@ -188,12 +195,7 @@
           (dom/div :.mid.column 
             (dom/div {:style {:text-align "center"}}
               (name id))
-            (dom/div {:onClick on-wormhole-click}
-              (dom/div :.spiral-wrapper {:style {:height "80%"
-                                                 :padding "5px"}}
-                (spiral [
-                         "wormhole-activated-animation"
-                         ]))))
+            (ui-wormhole (comp/computed props {:on-wormhole-click on-wormhole-click})))
           (dom/div :.right.column 
             (when items
               (dom/div :.items
@@ -258,7 +260,9 @@
    ;;:css [[:.space {:background "black"}]]
    }
   (dom/div 
-    (inj/style-element {:component Root})
+    (inj/style-element {:component Root
+                        ;; doesn't seem to be needed right now
+                        :garden-flags {:vendors [#_"webkit"]}})
     (ui-room-configuration (inspect room-configuration))))
 
 
